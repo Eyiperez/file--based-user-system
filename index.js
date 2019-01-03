6116399dffae4a5f474d923a4d443d5bd21c05ef
Erika Perez <erikaperezvalle@pursuit.org>
2019-01-02 21:53:35 -0500

    simple test succesful, express is working, server is listening on port 3000

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+   res.json({'message':'this is homepage'})
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
