9b2e6ddaa523eefc91a332764f5dc236e7d357f5
Erika Perez <erikaperezvalle@pursuit.org>
2019-01-02 22:04:02 -0500

    created the 4 required routes. ran a simple test in each which sends a response of a json message

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ app.get('/', (req, res) => {
    res.json({'message':'this is homepage'})
 });
 
+//Adding Students to a Class
+app.get('/class/add', (req, res) => {
+    res.json({'message':'add students and classes'})
+ });
+
+//List All Students in a Class
+ app.get('/class/list', (req, res) => {
+    res.json({'message':'show list of students per class'})
+ });
+
+//List Failing Students
+ app.get('/class/listfailing', (req, res) => {
+    res.json({'message':'show list of students failing in class'})
+ });
+
+ //List Students from a Specific City
+ app.get('/class/listfromcity', (req, res) => {
+    res.json({'message':'show students from a class from a specific city'})
+ });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
